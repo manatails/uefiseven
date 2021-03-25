08be72b9aab981b97c8855dd5df8205efe5728b0
cecekpawon <cecekpawon@users.noreply.github.com>
2021-03-25 18:48:34 +0700

    Display: Sources reformat

--- Platform/UefiSeven/Display.h
+++ Platform/UefiSeven/Display.h
@@ -41,53 +41,52 @@
 **/
 
 typedef struct {
-	UINTN			Width;
-	UINTN			Height;
-	EFI_UGA_PIXEL	*PixelData;
+  UINTN           Width;
+  UINTN           Height;
+  EFI_UGA_PIXEL   *PixelData;
 } IMAGE;
 
-typedef enum
-{
-	NONE,
-	GOP,
-	UGA
+typedef enum {
+  NONE,
+  GOP,
+  UGA
 } GRAPHICS_PROTOCOL;
 
 typedef struct {
-	BOOLEAN							Initialized;
-	BOOLEAN							AdapterFound;
+  BOOLEAN                       Initialized;
+  BOOLEAN                       AdapterFound;
 
-	GRAPHICS_PROTOCOL				Protocol;
-	EFI_UGA_DRAW_PROTOCOL			*UGA;
-	EFI_GRAPHICS_OUTPUT_PROTOCOL	*GOP;
+  GRAPHICS_PROTOCOL             Protocol;
+  EFI_UGA_DRAW_PROTOCOL         *UGA;
+  EFI_GRAPHICS_OUTPUT_PROTOCOL  *GOP;
 
-	UINT32							HorizontalResolution;
-	UINT32							VerticalResolution;
-	EFI_GRAPHICS_PIXEL_FORMAT		PixelFormat;
-	UINT32							PixelsPerScanLine;
-	EFI_PHYSICAL_ADDRESS			FrameBufferBase;
-	UINTN							FrameBufferSize;
+  UINT32                        HorizontalResolution;
+  UINT32                        VerticalResolution;
+  EFI_GRAPHICS_PIXEL_FORMAT     PixelFormat;
+  UINT32                        PixelsPerScanLine;
+  EFI_PHYSICAL_ADDRESS          FrameBufferBase;
+  UINTN                         FrameBufferSize;
 } DISPLAY_INFO;
 
 #pragma pack(1)
 typedef struct {
-	// File header
-	CHAR8	Signature[2];
-	UINT32	FileSizeBytes;
-	UINT16	Reserved[2];
-	UINT32	PixelDataOffset;
-	// DIB header
-	UINT32	DibHeaderSize;
-	UINT32	Width;
-	UINT32	Height;
-	UINT16	Planes;				// expect '1'
-	UINT16	BitPerPixel;		// expect '24' for 24bpp
-	UINT32	CompressionType;	// expect '0' for no compression
-	UINT32	ImageSize;			// size of the raw bitmap data
-	UINT32	XPixelsPerMeter;
-	UINT32	YPixelsPerMeter;
-	UINT32	NumberOfColors;
-	UINT32	ImportantColors;	// ignored
+  // File header
+  CHAR8     Signature[2];
+  UINT32    FileSizeBytes;
+  UINT16    Reserved[2];
+  UINT32    PixelDataOffset;
+  // DIB header
+  UINT32    DibHeaderSize;
+  UINT32    Width;
+  UINT32    Height;
+  UINT16    Planes;           // expect '1'
+  UINT16    BitPerPixel;      // expect '24' for 24bpp
+  UINT32    CompressionType;  // expect '0' for no compression
+  UINT32    ImageSize;        // size of the raw bitmap data
+  UINT32    XPixelsPerMeter;
+  UINT32    YPixelsPerMeter;
+  UINT32    NumberOfColors;
+  UINT32    ImportantColors;  // ignored
 } BMP_HEADER;
 #pragma pack()
 
@@ -99,74 +98,85 @@ typedef struct {
 **/
 
 VOID
-PrintVideoInfo(
-	VOID);
+PrintVideoInfo (
+  VOID
+  );
 
 VOID
-ClearScreen(
-	VOID);
+ClearScreen (
+  VOID
+  );
 
 VOID
-SwitchToText(
-	IN	BOOLEAN	Force);
+SwitchToText (
+  IN  BOOLEAN   Force
+  );
 
 EFI_STATUS
-SwitchVideoMode(
-	IN UINTN Width,
-	IN UINTN Height
-);
+SwitchVideoMode (
+  IN UINTN  Width,
+  IN UINTN  Height
+  );
 
 EFI_STATUS
-ForceVideoModeHack(
-	IN UINTN Width,
-	IN UINTN Height
-);
+ForceVideoModeHack (
+  IN UINTN  Width,
+  IN UINTN  Height
+  );
 
 BOOLEAN
-MatchCurrentResolution(
-	IN UINTN Width,
-	IN UINTN Height
-);
+MatchCurrentResolution (
+  IN UINTN  Width,
+  IN UINTN  Height
+  );
 
 VOID
-SwitchToGraphics(
-	IN	BOOLEAN	Force);
+SwitchToGraphics (
+  IN  BOOLEAN   Force
+  );
 
-VOID DestroyImage(
-	IN	IMAGE	*Image);
+VOID DestroyImage (
+  IN  IMAGE   *Image
+  );
 
-IMAGE*
-CreateImage(
-	IN	UINTN	Width,
-	IN	UINTN	Height);
+IMAGE *
+CreateImage (
+  IN  UINTN   Width,
+  IN  UINTN   Height
+  );
 
 EFI_STATUS
-BmpFileToImage(
-	IN	UINT8	*FileData,
-	IN	UINTN	FileSizeBytes,
-	OUT	VOID	**Result);
+BmpFileToImage (
+  IN  UINT8   *FileData,
+  IN  UINTN   FileSizeBytes,
+  OUT VOID    **Result
+  );
 
 VOID
-DrawImage(
-	IN	IMAGE	*Image,
-	IN	UINTN	Width,
-	IN	UINTN	Height,
-	IN	UINTN	ScreenX,
-	IN	UINTN	ScreenY,
-	IN	UINTN	ImageX,
-	IN	UINTN	ImageY);
+DrawImage (
+  IN  IMAGE   *Image,
+  IN  UINTN   Width,
+  IN  UINTN   Height,
+  IN  UINTN   ScreenX,
+  IN  UINTN   ScreenY,
+  IN  UINTN   ImageX,
+  IN  UINTN   ImageY
+  );
 
 VOID
-DrawImageCentered(
-	IN	IMAGE	*Image);
+DrawImageCentered (
+  IN  IMAGE   *Image
+  );
 
 VOID
-AnimateImage(
-	IN	IMAGE	*Image);
+AnimateImage (
+  IN  IMAGE   *Image
+  );
 
 EFI_STATUS
-EnsureDisplayAvailable(
-	VOID);
+EnsureDisplayAvailable (
+  VOID
+  );
 
 
 /**
@@ -175,7 +185,7 @@ EnsureDisplayAvailable(
   -----------------------------------------------------------------------------
 **/
 
-extern	DISPLAY_INFO					DisplayInfo;
+extern DISPLAY_INFO  DisplayInfo;
 
 
 #endif
